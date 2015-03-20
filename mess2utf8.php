d7d3c081a5150e43addcd995c16f7abc73a8fc72
Sam Trenholme <strenholme.usenet@gmail.com>
2015-03-19 19:18:47 -0700

    OK, UTF-8 in -> UTF-8 out for basic stuff

--- mess2utf8.php
+++ mess2utf8.php
@@ -27,6 +27,8 @@ function mess2utf8($in) {
 						($value & 0x3f) | 0x80);
 			} else if($value <= 0x7f) {
 				$out .= chr($value);
+			} else {
+				$out .= "¿";
 			}
 		}
 		return $out;
@@ -47,7 +49,34 @@ function mess2utf8($in) {
 			}
 			$out .= chr($value);
 			$buffer = "";
-		} 	
+			$state = 0;
+		} else if($s1 == 1) {
+			if($state == 0) {
+				$out .= cp1252_to_utf8(chr($value));
+				$buffer = "";
+			} else {
+				$state--;
+				$buffer .= chr($value);
+				if($state == 0) {
+					$out .= $buffer;
+					$buffer = "";
+				}
+			}
+		} else if($s1 <= 6 && $s1 > 1) {
+			if($state != 0) {
+				$out .= cp1252_to_utf8($buffer);
+			}
+			# While UTF-8 currently only allows sequences
+			# one to four bytes long, we will keep the 
+			# original spec which allows sequences up to
+			# six bytes long
+			$state = $s1 - 1;	
+			$buffer = chr($value);
+		} else {
+			$out .= cp1252_to_utf8($buffer);
+			$buffer = "";
+		}
+			
 	}
 
 	return $out;
